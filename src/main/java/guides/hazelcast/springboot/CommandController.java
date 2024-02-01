0434f5624267dbaf0334ebd5ef90582061936661
Oliver Brüntje <oliver.bruentje@vanderlande.com>
2024-02-01 13:35:14 +0100

    Client working with port-forward

--- src/main/java/guides/hazelcast/springboot/CommandController.java
+++ src/main/java/guides/hazelcast/springboot/CommandController.java
@@ -1,6 +1,7 @@
 package guides.hazelcast.springboot;
 
 import com.hazelcast.core.HazelcastInstance;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
